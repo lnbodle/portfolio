d228cf8ba603fdd35a6509ed3724b5f5cfb42509
lnbodle <lino.blondelle@outlook.fr>
2023-12-07 16:20:16 +0100

    Test for deployement build

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,7 @@ import Game from "./engine/Game.ts"
 
 export function setupGame(element: HTMLDivElement, joystick: HTMLElement) {
     
-    let game = new Game(element, joystick);
+    new Game(element, joystick);
     console.log("just for test")
 }
   
