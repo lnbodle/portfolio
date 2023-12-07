cac722334c85069262cbd9de485c7d4f48a41988
lnbodle <33087397+lnbodle@users.noreply.github.com>
2023-12-07 16:05:43 +0000

    Build: (dc58dd8) Test for deployement build

--- src/game.ts
+++
@@ -1,8 +0,0 @@
-import Game from "./engine/Game.ts"
-
-export function setupGame(element: HTMLDivElement, joystick: HTMLElement) {
-    
-    new Game(element, joystick);
-    console.log("just for test")
-}
-  
